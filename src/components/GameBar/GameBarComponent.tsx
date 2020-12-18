bdd0c36a5c9fc00783a0a238cf125f003d63cad2
valantynrocher <valentinrocher@gmail.com>
2020-12-18 20:43:10 +0100

    display level into GameBar

--- src/components/GameBar/GameBarComponent.tsx
+++ src/components/GameBar/GameBarComponent.tsx
@@ -1,25 +1,42 @@
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import React, { useContext, useMemo } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import { AppContext } from "../../context";
 import { useStyles } from "./styles";
 import PauseIcon from "@material-ui/icons/Pause";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import StopIcon from "@material-ui/icons/Stop";
+import RotateLeftIcon from "@material-ui/icons/RotateLeft";
 import SettingsIcon from "@material-ui/icons/Settings";
 
 const GameBarComponent = () => {
-  const { state, togglePlaying } = useContext(AppContext);
+  const { state, togglePlaying, reset } = useContext(AppContext);
 
   const classes = useStyles();
 
+  const getLevel = useCallback(() => {
+    const speed = state.speed;
+    if (speed <= 400 && speed >= 301) {
+      return "Easy";
+    } else if (speed <= 300 && speed >= 201) {
+      return "Intermediate";
+    } else if (speed <= 200 && speed >= 101) {
+      return "Pro";
+    } else if (speed <= 100) {
+      return "the goat";
+    }
+    return "unknow";
+  }, [state.speed]);
+
   const events = useMemo(() => {
     return {
-      handleStopClick: () => {
+      handlePause: () => {
         togglePlaying();
       },
+      handleReset: () => {
+        reset();
+      },
     };
-  }, [togglePlaying]);
+  }, [reset, togglePlaying]);
 
   return (
     <div className={classes.root}>
@@ -28,19 +45,31 @@ const GameBarComponent = () => {
           <Typography color="primary" variant="caption">
             score :
           </Typography>
-          <Typography color="primary" variant="h4">
+          <Typography color="primary" variant="h6">
             {state.points}
+          </Typography>
+        </div>
+        <div className={classes.points}>
+          <Typography color="primary" variant="caption">
+            level :
+          </Typography>
+          <Typography color="primary" variant="h6">
+            {getLevel()}
           </Typography>
         </div>
         <IconButton
           classes={{ root: classes.buttonRoot }}
-          onClick={events.handleStopClick}
+          onClick={events.handlePause}
           color="primary"
         >
           {state.isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
         </IconButton>
-        <IconButton classes={{ root: classes.buttonRoot }} color="primary">
-          <StopIcon />
+        <IconButton
+          onClick={events.handleReset}
+          classes={{ root: classes.buttonRoot }}
+          color="primary"
+        >
+          <RotateLeftIcon />
         </IconButton>
       </div>
       <div className={classes.flexCenter}>

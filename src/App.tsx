3b79898a7d154a91cddd32fcb42d500baf8025e7
valantynrocher <valentinrocher@gmail.com>
2020-12-18 18:00:51 +0100

    implement custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,92 @@
 import React from "react";
 import Playground from "./components/Playground";
 import Provider from "./context/Provider";
+import {
+  createMuiTheme,
+  createStyles,
+  makeStyles,
+  Theme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import GameBar from "./components/GameBar";
+import { amber, teal } from "@material-ui/core/colors";
+import BackdropComponent from "./components/Backdrop/BackdropComponent";
+
+const font = "'Ultra', serif";
+
+let defaultTheme = createMuiTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: teal[700],
+    },
+    secondary: amber,
+  },
+  typography: {
+    fontSize: 14,
+    fontFamily: font,
+    h1: {
+      textTransform: "uppercase",
+    },
+    h2: {
+      textTransform: "uppercase",
+    },
+    h3: {
+      textTransform: "uppercase",
+    },
+    h4: {
+      textTransform: "uppercase",
+    },
+    h5: {
+      textTransform: "uppercase",
+    },
+    h6: {
+      textTransform: "uppercase",
+    },
+    subtitle1: {
+      textTransform: "uppercase",
+    },
+    subtitle2: {
+      textTransform: "uppercase",
+    },
+  },
+});
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    app: {
+      backgroundColor: theme.palette.grey[300],
+      height: "100vh",
+      overflow: "hidden",
+    },
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      minHeight: "100vh",
+    },
+  })
+);
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Provider>
-      <Playground />
+      <ThemeProvider theme={defaultTheme}>
+        <div className={classes.app}>
+          <Container
+            className={classes.container}
+            disableGutters
+            fixed
+            maxWidth="sm"
+          >
+            <GameBar />
+            <Playground />
+          </Container>
+          <BackdropComponent />
+        </div>
+      </ThemeProvider>
     </Provider>
   );
 };
